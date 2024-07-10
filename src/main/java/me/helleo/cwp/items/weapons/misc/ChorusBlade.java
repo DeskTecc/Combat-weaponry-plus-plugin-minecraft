89a6b6a9432e8646b561cd9d8d3d6d5691d0b3d1
DeskTecc <techseven167@gmail.com>
2024-07-10 13:51:30 -0300

    saving progress

--- src/main/java/me/helleo/cwp/items/weapons/misc/ChorusBlade.java
+++ src/main/java/me/helleo/cwp/items/weapons/misc/ChorusBlade.java
@@ -1,13 +1,17 @@
 package me.helleo.cwp.items.weapons.misc;
 
+import me.helleo.cwp.CombatWeaponryPlus;
 import me.helleo.cwp.configurations.ConfigurationsBool;
 import me.helleo.cwp.configurations.ConfigurationsDouble;
+import me.helleo.cwp.configurations.ConfigurationsString;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.EquipmentSlotGroup;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -23,8 +27,24 @@ public class ChorusBlade {
 
     public static ItemStack getTool(){
 
+        //modifier
+        double dmg = 3;
+        double spd = 6;
+        if (ConfigurationsBool.UseCustomValues.getValue()) {
+            dmg = ConfigurationsDouble.Swords_ChorusBlade_Damage.getValue();
+            spd = ConfigurationsDouble.Swords_ChorusBlade_Speed.getValue();
+        }
+        AttributeModifier modifier = new AttributeModifier(new NamespacedKey(CombatWeaponryPlus.plugin, "Attack Speed"), spd,
+                AttributeModifier.Operation.ADD_NUMBER, EquipmentSlotGroup.HAND);
+        meta.addAttributeModifier(Attribute.GENERIC_ATTACK_SPEED, modifier);
+        AttributeModifier modifier2 = new AttributeModifier(new NamespacedKey(CombatWeaponryPlus.plugin, "Attack Damage"), dmg,
+                AttributeModifier.Operation.ADD_NUMBER, EquipmentSlotGroup.HAND);
+        meta.addAttributeModifier(Attribute.GENERIC_ATTACK_DAMAGE, modifier2);
 
-        meta.setDisplayName(ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("dChorusBlade.name")));
+        meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+
+
+        meta.setDisplayName(ChatColor.translateAlternateColorCodes('&', ConfigurationsString.DescriptionChorusBlade_Name.getValue()));
 
         if (ConfigurationsBool.EnchantsChorusBlade.getValue()) {
             int unbreakingLevel = (int) ConfigurationsDouble.ChorusEnchantLevels_Unbreaking.getValue();
@@ -33,37 +53,21 @@ public class ChorusBlade {
             meta.addEnchant(Enchantment.KNOCKBACK, knockbackLevel, true);
         }
         List<String> lore = new ArrayList<String>();
-        lore.add(ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("dChorusBlade.line1")));
-        lore.add(ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("dChorusBlade.line2")));
-        lore.add(ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("dChorusBlade.line3")));
-        lore.add(ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("dChorusBlade.line4")));
-        lore.add(ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("dChorusBlade.line5")));
-        lore.add(ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("dChorusBlade.line6")));
-        lore.add(ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("dChorusBlade.line7")));
-        lore.add(ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("dChorusBlade.line8")));
-        lore.add(ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("dChorusBlade.line9")));
+        lore.add(ChatColor.translateAlternateColorCodes('&', ConfigurationsString.DescriptionChorusBlade_Line1.getValue()));
+        lore.add(ChatColor.translateAlternateColorCodes('&', ConfigurationsString.DescriptionChorusBlade_Line2.getValue()));
+        lore.add(ChatColor.translateAlternateColorCodes('&', ConfigurationsString.DescriptionChorusBlade_Line3.getValue()));
+        lore.add(ChatColor.translateAlternateColorCodes('&', ConfigurationsString.DescriptionChorusBlade_Line4.getValue()));
+        lore.add(ChatColor.translateAlternateColorCodes('&', ConfigurationsString.DescriptionChorusBlade_Line5.getValue()));
+        lore.add(ChatColor.translateAlternateColorCodes('&', ConfigurationsString.DescriptionChorusBlade_Line6.getValue()));
+        lore.add(ChatColor.translateAlternateColorCodes('&', ConfigurationsString.DescriptionChorusBlade_Line7.getValue()));
+        lore.add(ChatColor.translateAlternateColorCodes('&',"&9 "+dmg+" Attack Damage"));
+        lore.add(ChatColor.translateAlternateColorCodes('&', "&9 "+spd+" Attack Speed"));
         meta.setLore(lore);
         //important:
         meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
         meta.setLore(lore);
-
-        //modifier
-        double dmg = 3;
-        double spd = 6;
-        if (this.getConfig().getString("UseCustomValues") == "true") {
-            dmg = this.getConfig().getDouble("aChorusBlade.damage") - 1;
-            spd = this.getConfig().getDouble("aChorusBlade.speed") - 4;
-        }
-        AttributeModifier modifier = new AttributeModifier(UUID.randomUUID(), "Attack Speed", spd,
-                AttributeModifier.Operation.ADD_NUMBER, EquipmentSlot.HAND);
-        meta.addAttributeModifier(Attribute.GENERIC_ATTACK_SPEED, modifier);
-        AttributeModifier modifier2 = new AttributeModifier(UUID.randomUUID(), "Attack Damage", dmg,
-                AttributeModifier.Operation.ADD_NUMBER, EquipmentSlot.HAND);
-        meta.addAttributeModifier(Attribute.GENERIC_ATTACK_DAMAGE, modifier2);
-
-        meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
-
         meta.setCustomModelData(1000007);
         item.setItemMeta(meta);
+        return item;
     }
 }
